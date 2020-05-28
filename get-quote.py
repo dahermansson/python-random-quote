ca913e33e25587f12a0d629bc0d71b965370de64
Daniel <daniel.hermansson@gmail.com>
2020-05-28 22:37:54 +0200

    done

--- get-quote.py
+++ get-quote.py
@@ -8,8 +8,19 @@ def hepp():
   f.close()
 
   last = len(quotes) - 1
-  rnd = random.randint(0, last)
-  print(quotes[rnd])
+  i = 0
+  while i <3:
+    rnd = random.randint(0, last)
+    print(quotes[rnd].rstrip())
+    i += 1
+
+def add(newQuote):
+  f = open("quotes.txt", "a")
+  f.write(newQuote)
+  f.close
+
 
 if __name__== "__main__":
   hepp()
+  newQuote = input()
+  add(newQuote)
